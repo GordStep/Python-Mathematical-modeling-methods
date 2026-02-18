a65f43dd9b7df2b6930e22ea8704711a2890466d
GordStep <gordeystepushov@gmail.com>
2026-02-18 21:42:01 +0300

    finish zad_1

--- source/dihotomi.cpp
+++ source/dihotomi.cpp
@@ -10,7 +10,7 @@ double f(float x) {
 double dihotomi(double a, double b, double eps) 
 {
     double c = 0;
-
+    int count_iter = 0;
     do
     {
         c = (a + b) / 2;
@@ -23,8 +23,11 @@ double dihotomi(double a, double b, double eps)
         {
             a = c;
         }
+        count_iter++;
         //cout << "a: " << a << "\nb: " << b << "\nc: " << c << "\nf: " << f(c) << endl; 
     } while (fabs(f(c)) > eps);
+
+    cout << "Метод дихотомии: x0 = "<< c << ", f(x0) = " << f(c) << endl << "Количество итераций: " << count_iter << endl << endl;
 
     return c;
 }
@@ -42,7 +45,7 @@ double dotOfLine(double x_1, double x_2)
 double MethodHords(double a, double b, double eps) 
 {
     double c = 0;
-
+    int count_iter = 0;
     do
     {
         c = dotOfLine(a, b);
@@ -55,16 +58,25 @@ double MethodHords(double a, double b, double eps)
         {
             a = c;
         }
+        count_iter++;
         //cout << "a: " << a << "\nb: " << b << "\nc: " << c << "\nf: " << f(c) << endl; 
     } while (fabs(f(c)) > eps);
+
+    cout << "Метод хорд: x0 = "<< c << ", f(x0) = " << f(c) << endl << "Количество итераций: " << count_iter << endl << endl;
 
     return c;
 }
 
 int main()
 {
-    cout << dihotomi(-5, 0, 0.0001) << endl;
-    cout << MethodHords(0, 5, 0.0001) << endl;
+    double a = -5.0, b = 0.0, eps = 0.0001;
+
+    cout << "Отрезок: (" << a << ", " << b << ")" << endl << endl;
+
+    double x_dih = dihotomi(a, b, eps);
+    double x_hord = MethodHords(a, b, eps);
+
+    cout << "Дельта методов: по x = " << fabs(f(x_dih) - f(x_hord)) << endl;
 
     return 0;
 }
